d9d4f0038bc4459b2144ee9fbe759750937ffc6e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-05-11 01:07:17 +0000

    Change title

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'stefanknapp.art',
+    title: 'Stefan Knapp Estate',
     htmlAttrs: {
       lang: 'en'
     },
