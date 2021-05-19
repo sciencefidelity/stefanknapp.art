eb82d27c52f0e02757e2090826797de2580fabde
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-05-19 15:33:53 +0000

    Fix image positioning, update classes to use BEM

--- web/src/components/SanityImage.vue
+++ web/src/components/SanityImage.vue
@@ -82,6 +82,7 @@ export default {
   position: relative;
   height: 100%;
   width: 100%;
+  overflow: hidden;
 }
 
 img {
@@ -94,6 +95,8 @@ img {
   position: absolute;
   top: 0;
   left: 0;
+  bottom: 0;
+  right: 0;
   z-index: 1;
 }
 
---
+++ web/src/pages/Beginnings.vue
@@ -0,0 +1,212 @@
+<template>
+  <layout
+    :title="$static.page.mainImage.caption.en"
+    :link="$static.page.mainImage"
+    :width="$static.page.mainImage.asset.metadata.dimensions.width"
+    :height="$static.page.mainImage.asset.metadata.dimensions.height"
+  >
+    <main>
+      <section>
+        <div class="container gallery">
+          <div v-for="edge in $static.artwork.edges" :key="edge.node.id">
+            <div class="gallery__image">
+              <sanity-image
+                :title="edge.node.title.en"
+                :link="edge.node.mainImage"
+                :width="edge.node.mainImage.asset.metadata.dimensions.width"
+                :height="edge.node.mainImage.asset.metadata.dimensions.height"
+                w=400
+                h=400
+              />
+            </div>
+            <p v-if="$context.locale === 'en-gb'" class="gallery__caption">
+              {{ edge.node.title.en }} ({{ edge.node.date }})<br />
+              {{ edge.node.medium[0].title.en }}
+            </p>
+            <p v-else class="caption">
+              {{ edge.node.title.en }},<br />
+              {{ edge.node.medium[0].title.pl }}, {{ edge.node.date }}
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="section--text">
+        <div class="container text" v-if="$static.page.body">
+          <h2>{{ $static.page.title.en }}</h2>
+          <div>
+            <block-content
+              v-if="$context.locale === 'en-gb'"
+              :blocks="$static.page.body._rawEn"
+            />
+            <block-content
+              v-else
+              :blocks="$static.page.body._rawPl"
+            />
+          </div>
+        </div>
+        <div class="text__links">
+          <g-link to="/art/">&lt; Back to Art</g-link>
+          <g-link to="/early-phase/">Next: Early Phase (1947-1953) &gt;</g-link>
+        </div>
+      </section>
+    </main>
+  </layout>
+</template>
+
+<static-query>
+query {
+  page: sanityPeriod(id: "87c31a8d-be84-4519-951d-488798e2955e") {
+    id
+    title {
+      en
+    }
+    body {
+      _rawEn(resolveReferences: {maxDepth: 5})
+      _rawPl(resolveReferences: {maxDepth: 5})
+    }
+    mainImage {
+      caption {
+      en}
+      asset {
+        _id
+        url
+        metadata {
+          dimensions {
+            height
+            width
+          }
+        }
+      }
+    }
+  }
+  artwork: allSanityArtwork(sortBy: "date", order: ASC, filter: { date: { lte: 1946 }}) {
+    edges {
+      node {
+        id
+        title {
+          en
+          pl
+        }
+        medium {
+          title {
+            en
+            pl
+          }
+        }
+        date
+        mainImage {
+          asset {
+            _id
+            url
+            metadata {
+              dimensions {
+                height
+                width
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+</static-query>
+
+<script lang="ts">
+import SanityImage from '@/components/SanityImage.vue'
+import BlockContent from '@/components/BlockContent.vue'
+
+export default {
+  name: 'Beginings',
+  components: {
+    SanityImage,
+    BlockContent
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/scss/colors' as c;
+@use '../assets/scss/breakpoints' as b;
+
+main {
+  background: c.$grey-000;
+}
+
+h2 {
+  margin-bottom: 3rem;
+}
+
+.container {
+  width: min(100rem, 88%);
+  margin: auto;
+}
+
+// ***** gallery section *****
+
+.gallery {
+  padding: 14rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 1em;
+  @include b.mq(md) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @include b.mq(sm) {
+    grid-template-columns: 1fr;
+  }
+  div {
+    margin-bottom: 1em;
+  }
+  p {
+    margin-bottom: 0;
+  }
+  &__image {
+    margin-bottom: 0.5em !important;
+    aspect-ratio: 1 / 1;
+    transition: 0.4s opacity;
+    &:hover {
+      opacity: 90%;
+      transition: 0.4s opacity;
+    }
+  }
+  &__caption {
+    text-align: right;
+    font-size: 1.8rem;
+  }
+}
+
+// ***** text section *****
+
+.section--text {
+  position: relative;
+  padding: 15rem 0 18rem;
+  background: c.$slate-100;
+  z-index: 0;
+}
+
+.text {
+  column-count: 2;
+  column-gap: 4rem;
+  column-width: min(53ch, 100%);
+  @include b.mq(lg) {
+    column-count: 1;
+    width: min(53ch, 88%);
+  }
+  &__links {
+    width: min(100rem, 88%);
+    margin: 2em auto;
+    display: flex;
+    justify-content: space-between;
+    a {
+      display: block;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+}
+
+</style>
