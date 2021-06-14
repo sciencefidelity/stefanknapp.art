ee3e45be51d425c1d9a53c1e4de6998207ef62e3
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-14 15:17:28 +0000

    Add schema document

--- studio/schemas/schema.ts
+++ studio/schemas/schema.ts
@@ -16,6 +16,7 @@ import artwork from './documents/artwork'
 import bio from './documents/bio'
 import meta from './documents/meta'
 import page from './documents/page'
+import period from './documents/period'
 import photography from './documents/photography'
 import video from './documents/video'
 
@@ -38,6 +39,7 @@ export default createSchema({
     bio,
     meta,
     page,
+    period,
     photography,
     video,
 
