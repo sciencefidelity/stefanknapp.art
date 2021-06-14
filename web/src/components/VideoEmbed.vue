e35c6c52b7155b73c9f923f9a634245e7f32b980
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-14 22:32:32 +0000

    Embed video from sanity

--- web/src/components/VideoEmbed.vue
+++ web/src/components/VideoEmbed.vue
@@ -6,8 +6,14 @@
     playsinline
     :class="classname"
   >
-    <source type="video/mp4" src="../../static/03-hc.mp4" />
-    <source type="video/webm" src="../../static/03-hc.webm" />
+    <source
+      :src="$static.sanityVideo.mp4.asset.url"
+      type="video/mp4"
+    />
+    <source
+      :src="$static.sanityVideo.webm.asset.url"
+      type="video/webm"
+    />
     <picture>
       <source srcset="../../static/03-hc.webp" type="image/webp">
       <source srcset="../../static/03-hc.jpg" type="image/jpeg">
@@ -19,14 +25,44 @@
   </video>
 </template>
 
+<static-query>
+  query {
+    sanityVideo(id: "ae87b16a-585e-488b-99bb-b00cf9145621") {
+      id
+      title {
+        en
+        pl
+      }
+      mp4 {
+        asset {
+          url
+        }
+      }
+      webm {
+        asset {
+          url
+        }
+      }
+      mainImage {
+        asset {
+          _id
+          url
+          metadata {
+            dimensions {
+              height
+              width
+            }
+          }
+        }
+      }
+    }
+  }
+</static-query>
+
 <script lang="ts">
+
 export default {
   name: 'VideoEmbed',
-  data() {
-    return {
-      video: '09-hc'
-    }
-  },
   props: {
     classname: {
       type: String,
