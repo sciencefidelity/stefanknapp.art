179f607a5405dced7d4e905971e945b6669cf6ac
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-18 23:59:14 +0000

    feat: start to set up i18n

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,6 +1,21 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from "nuxt3"
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    locales: ["en", "pl"],
+    defaultLocale: "en",
+    vueI18n: {
+      fallbackLocale: "en",
+      messages: {
+        en: {
+          welcome: "Welcome"
+        },
+        pl: {
+          welcome: "Witamy"
+        }
+      }
+    }
+  }
 })
