32ab1af5aa0086682f1b5327b744cfab7e2f57a8
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-20 12:25:54 +0000

    refactor: move sanity config to Nuxt config file

--- nuxt/sanityClient.ts
+++
@@ -1,9 +0,0 @@
-import sanityClient from "@sanity/client"
-// const config = useRuntimeConfig()
-
-export default sanityClient({
-  projectId: "0u3fubc7",
-  dataset: "production",
-  apiVersion: "2021-08-31",
-  useCdn: true
-})
