cb189ecc211737f95133c9ab87221db3b2ec6fea
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-20 13:43:33 +0000

    build: add Nuxt2 template

---
+++ nuxt3/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+root: true,
+env: {
+  browser: true,
+  node: true
+},
+extends: [
+  "@nuxtjs/eslint-config-typescript",
+  "plugin:nuxt/recommended",
+  "prettier"
+],
+plugins: [],
+// add your custom rules here
+rules: {
+  indent: ["error", 2],
+  "linebreak-style": ["error", "unix"],
+  quotes: ["error", "double"],
+  semi: ["error", "never"]
+}
+}
