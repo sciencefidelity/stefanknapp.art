0077ddd345d5287f92cb9228ed91f66ae8a9b823
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-20 13:39:24 +0000

    build: add Nuxt2 template

---
+++ nuxt2/stylelint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-prettier",
+    "stylelint-config-sass-guidelines",
+  ],
+  "plugins": ["stylelint-scss"],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    "color-no-hex": true,
+    "max-nesting-depth": 4,
+    "rule-empty-line-before": ["always", { except: ["inside-block"] }],
+    "selector-max-id": 1,
+  },
+}
