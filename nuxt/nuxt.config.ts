84d3ff830ede1558e7402206ddb454b4af2f820e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-20 12:24:51 +0000

    build: add support for stylelint

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,42 +2,18 @@ import { defineNuxtConfig } from "nuxt3"
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: [
-    '@/assets/main.scss'
+  target: "static",
+
+  css: ["assets/main.scss"],
+
+  buildModules: [
+    "@nuxtjs/eslint-module",
+    "@nuxtjs/sanity/module",
+    "@nuxtjs/stylelint-module"
   ],
-  // publicRuntimeConfig: {
-  //   sanityProjectId: process.env.SANITY_PROJECT_ID,
-  //   sanityDataset: process.env.SANITY_DATASET
-  // },
-  buildModules: ['@intlify/nuxt3'],
-  intlify: {
-    locales: [
-      {
-        code: "en",
-        iso: "en-GB"
-      },
-      {
-        code: "es",
-        iso: "pl-PL"
-      }
-    ],
-    baseUrl: "https://stefanknapp.art",
-    defaultLocale: "en",
-    vueI18n: {
-      fallbackLocale: "en",
-      messages: {
-        en: {
-          welcome: "Welcome"
-        },
-        pl: {
-          welcome: "Witamy"
-        }
-      }
-    },
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieKey: "i18n_redirected",
-      redirectOn: "root"
-    }
+
+  sanity: {
+    dataset: "production",
+    projectId: "0u3fubc7"
   }
 })
