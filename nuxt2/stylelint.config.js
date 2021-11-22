aeb6a366337d9b405bcfda5d823d695a5aa13658
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-22 07:41:39 +0000

    build: add support for stylelint-config-recess-order

--- nuxt2/stylelint.config.js
+++ nuxt2/stylelint.config.js
@@ -1,19 +1,27 @@
 module.exports = {
   extends: [
-    "stylelint-config-standard",
     "stylelint-config-prettier",
+    "stylelint-config-standard",
     "stylelint-config-sass-guidelines",
+    "stylelint-config-recess-order"
   ],
-  "plugins": ["stylelint-scss"],
+  plugins: ["stylelint-order", "stylelint-scss"],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
+    "at-rule-empty-line-before": [
+      "always",
+      {
+        except: ["blockless-after-blockless", "inside-block"]
+      }
+    ],
     "at-rule-no-unknown": null,
-    "scss/at-rule-no-unknown": true,
     "color-no-hex": true,
     "max-nesting-depth": 4,
     "rule-empty-line-before": ["always", { except: ["inside-block"] }],
     "selector-max-id": 1,
-    "value-keyword-case": ["lower", { "ignoreProperties": ["/^\\$/"] }]
-  },
+    "value-keyword-case": ["lower", { ignoreProperties: ["/^\\$/"] }],
+    "order/properties-alphabetical-order": false,
+    "scss/at-rule-no-unknown": true
+  }
 }
