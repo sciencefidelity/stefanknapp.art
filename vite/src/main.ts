24231e5068160ebb204d48ff12dcc3eadc3aa13b
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-13 22:16:21 +0100

    feat: set up vue i18n

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import { createI18n } from "vue-i18n"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+  // something vue-i18n options here ...
+})
+
+const app = createApp({})
+
+app.use(i18n)
+app.mount("#app")
