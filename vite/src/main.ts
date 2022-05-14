4295d60c84ea75d399da7cc22c4c46b1eb84a2f6
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-14 13:47:50 +0100

    feat: set up router and add routes

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import { createHead } from "@vueuse/head"
 import { createI18n } from "vue-i18n"
 import messages from "@intlify/vite-plugin-vue-i18n/messages"
+import router from "./router/index"
 import App from "./App.vue"
 import "modern-normalize"
 import "styles/global.scss"
@@ -18,4 +19,5 @@ const app = createApp(App)
 
 app.use(i18n)
 app.use(head)
+app.use(router)
 app.mount("#app")
---
+++ vite/src/views/Estate.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Estate</h1>
+</template>
